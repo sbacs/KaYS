db9771a3df6a82a5a4ab9155d324dda2361c7b99
sbacs <tommasoluciani07@gmail.com>
2026-04-13 23:45:32 +0200

    changes api

--- app/articoli/page.tsx
+++ app/articoli/page.tsx
@@ -1,2 +1,18 @@
+import { Articolo } from "../lib/types"
+import { getArticoli } from "../services/articoli"
 
-export default function {}
+
+export default async function articolo() {
+
+    const articoli : Articolo[] = await getArticoli()
+
+    return (
+        <div className="flex flex-col gap-y-4 justify-center px-25 py-5">
+            {
+                articoli.map((a) => {
+                    return <div className="h-10 flex items-center rounded-lg  bg-white/5 px-5 ">{a.nome} + {a.fornitore} + {a.posizione} + {a.descrizione} + {a.quantitaRecipiente} </div>
+                })
+            }
+        </div>
+    )
+}
